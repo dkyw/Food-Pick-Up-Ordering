fe5ad5f5f028c0a44ed3ae42ce99432d011b1b83
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-25 22:45:34 +0000

    Add toggle sidebar to index page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,17 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });
+$(".sidebar").hide();
+
+  $(".checkout").on("click", function(event) {
+    event.preventDefault();
+    console.log("clicked");
+    $(".sidebar").toggle("slide");
+  });
 });
