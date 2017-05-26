84c6dfb5b7d5d638de3176a4a7b2af4a6b498831
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-26 19:33:57 +0000

    Add items table to the index page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,28 +7,11 @@ $(() => {
   //     $("<div>").text(user.name).appendTo($("body"));
   //   }
   // });
-  $.ajax({
-    method: "GET",
-    url: "/api/restaurants"
-  }).done((restaurants) => {
-    restaurants.forEach((ele)=> {
-      console.log(typeof ele.logo)
-     
-    var $restaurantLogo = $("<img>").attr({
-      id: "restaurantLogo",
-      alt: "Restaurant Logo",
-      src: ele.logo,
-      width: "117",
-      length: "71"
-    });
-    $(".navbar-fixed-top .container").prepend($restaurantLogo);
-    
-  
-  function landingPage() {
+  function landingPage(restaurant) {
     var $row = $("<div>").addClass("row");
     var $columnSize = $("<div>").addClass("col-md-6 col-sm-12");
-    var $restaurantName = $("<h1>").text(ele.name);
-    var $restaurantDescription = $("<p>").text(ele.description);
+    var $restaurantName = $("<h1>").text(restaurant.name);
+    var $restaurantDescription = $("<p>").text(restaurant.description);
 
     $row.append($columnSize);
     $columnSize.append($restaurantName, $restaurantDescription);
@@ -36,40 +19,25 @@ $(() => {
     return $row;
   }
 
-  $("#landingPage").append(landingPage());
-
-});
-
+  $.ajax({
+    method: "GET",
+    url: "/api/restaurants"
+  }).done(function(restaurants) {
+    console.log("restos",restaurants);
+    restaurants.forEach(function(restaurant) {
+      console.log("resto",restaurant);
+      
+      var $restaurantLogo = $("<img>").attr({
+        id: "restaurantLogo",
+        alt: "Restaurant Logo",
+        src: restaurant.logo,
+        width: "117",
+        length: "71"
+      });
+    $(".navbar-fixed-top .container").prepend($restaurantLogo);
+    $("#landingPage").append(landingPage(restaurant));
+   });
   });
-
-
-
-// Restaurant Logo
-  // var $restaurantLogo = $("<img>").attr({
-  //   id: "restaurantLogo",
-  //   alt: "Restaurant Logo",
-  //   src: "https://nodejs.org/static/images/logos/nodejs-new-pantone-white.png",
-  //   width: "117",
-  //   length: "71"
-  // });
-
-  // $(".navbar-fixed-top .container").prepend($restaurantLogo);
-
-// Creating the Landing Page
-  // function landingPage() {
-  //   // var $landingPage = $("#landingPage");
-  //   var $row = $("<div>").addClass("row");
-  //   var $columnSize = $("<div>").addClass("col-md-6 col-sm-12");
-  //   var $restaurantName = $("<h1>").text("restoName");
-  //   var $restaurantDescription = $("<p>").text("Describe");
-
-  //   $row.append($columnSize);
-  //   $columnSize.append($restaurantName, $restaurantDescription);
-
-  //   return $row;
-  // }
-
-  // $("#landingPage").append(landingPage()); 
 
 // Sidebar initially hidden, shown on toggle
   $(".sidebar").hide();
@@ -79,14 +47,13 @@ $(() => {
     $(".sidebar").toggle("slide");
   });
 
-// Food Items Section of the Page (Individual)
-  function eachFood(food) {
+ function eachFood(food) {
     var $food = $("<div>").addClass("col-xs-18 col-sm-6 col-md-4");
     var $thumbnail = $("<div>").addClass("thumbnail");
-    var $foodImage = $("<img>").addClass("foodImage").attr("src", "http://placehold.it/500x250/EEE");
+    var $foodImage = $("<img>").addClass("foodImage").attr("src", food.photo);
     var $caption = $("<div>").addClass("caption");
-    var $foodName = $("<h4>").text("Label");
-    var $foodDescription = $("<p>").text("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, soluta, eligendi doloribus sunt minus amet sit debitis repellat. Consectetur, culpa itaque odio similique suscipit");
+    var $foodName = $("<h4>").text(food.name);
+    var $foodDescription = $("<p>").text(food.description);
     var $button = `<a href="#" class="btn btn-default btn-xs pull-right" role="button">
       <i class="glyphicon glyphicon-edit"></i>
       </a> 
@@ -100,17 +67,30 @@ $(() => {
     return $food;
   }
 
-// Iterate each food entry
-  function renderFoods() {
-    var $foodsContainer = $("#foodItems");
-    var foods = [1,2,3,4,5,6];
-    for (var item in foods) {
-      // var food = foods[item];
-      var food = foods.length;
+
+// Food Items Section of the Page (Individual)
+$.ajax({
+    method: "GET",
+    url: "/api/items"
+  }).done(function(items) {
+    console.log(items)
+    items.forEach(function(food) {
+      console.log("food",food);
+      var $foodsContainer = $("#foodItems");
       $foodsContainer.append(eachFood(food));
-    }
-  }
-
-  renderFoods();
-
+  
+// // Iterate each food entry
+//   function renderFoods(foods) {
+//     var $foodsContainer = $("#foodItems");
+//     // var foods = [1,2,3,4,5,6];
+//     // for (var item in foods) {
+//       var food = items[food];
+//       // var food = foods.length;
+//       $foodsContainer.append(eachFood(food));
+//     }
+//   }
+// renderFoods(foods);
+  
+    });
+});
 });
