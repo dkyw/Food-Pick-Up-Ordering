c8c6d75632aaa74191933910718511e24f9f2828
Alexandra Sia <alexandrasia@gmail.com>
2017-05-27 02:59:42 +0000

    Create new seed data for items and restaurants tables

--- db/seeds/seed4_items.js
+++ db/seeds/seed4_items.js
@@ -2,9 +2,13 @@ exports.seed = function(knex, Promise) {
   return knex('items').del()
     .then(function () {
       return Promise.all([
-        knex('items').insert({id: 1, name: 'Sushi', description: 'Best Sushi in town.', amount: 4.99, photo: 'http://www.just-eat.ca/delivery/images/branding/cuisines/en_ca/thumb_sushi.png', restaurant_id: 1}),
-        knex('items').insert({id: 2, name: 'Sashimi', description: 'Fresh, not Frozen.', amount: 7.99, photo: 'https://s-media-cache-ak0.pinimg.com/736x/0b/33/c7/0b33c7f46a8f3ed943965f782cc8355c.jpg', restaurant_id: 1}),
-        knex('items').insert({id: 3, name: 'Burrito', description: 'BURRITO?! Surprised, huh?!.', amount: 6.99, photo: 'https://media-cdn.tripadvisor.com/media/photo-s/05/86/31/e0/truknyaki.jpg', restaurant_id: 1})
+        knex('items').insert({id: 1, name: 'Sashimi', description: 'Thinly sliced and fresh, not frozen', amount: 10.99, photo: 'https://media.timeout.com/images/102315343/1372/772/image.jpg', restaurant_id: 1}),
+        knex('items').insert({id: 2, name: 'Ramen', description: 'Shoyu (soy-based broth) with vegetables', amount: 8.99, photo: 'https://media.timeout.com/images/100611353/1372/772/image.jpg', restaurant_id: 1}),
+        knex('items').insert({id: 3, name: 'Yakitori', description: 'Marinated in soy sauce, sake and ginger', amount: 4.99, photo: 'https://media.timeout.com/images/100546753/1372/772/image.jpg', restaurant_id: 1}),
+        knex('items').insert({id: 4, name: 'Katsu', description: 'Crunchy on the outside and tender on the inside', amount: 9.99, photo: 'https://media.timeout.com/images/100546755/1372/772/image.jpg', restaurant_id: 1}),
+        knex('items').insert({id: 5, name: 'Sake', description: 'Imported from Japan', amount: 5.99, photo: 'https://media.timeout.com/images/100546759/1372/772/image.jpg', restaurant_id: 1}),
+        knex('items').insert({id: 6, name: 'Eggplant', description: 'Grilled eggplant with sesame and green onion', amount: 7.99, photo: 'https://media.timeout.com/images/100546779/1372/772/image.jpg', restaurant_id: 1}),
+        knex('items').insert({id: 7, name: 'Burrito', description: 'Best burrito in town!', amount: 6.99, photo: 'https://media.timeout.com/images/100678819/1372/772/image.jpg', restaurant_id: 1})
       ]);
     });
 };
