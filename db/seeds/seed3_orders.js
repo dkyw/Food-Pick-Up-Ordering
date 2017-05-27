012bae8ebff652ba61dddcd6e28b9ec558ae96a3
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-27 23:24:55 +0000

    Deleted, not needed

--- db/seeds/seed3_orders.js
+++
@@ -1,9 +0,0 @@
-exports.seed = function(knex, Promise) {
-  return knex('orders').del()
-    .then(function () {
-      return Promise.all([
-        knex('orders').insert({id: 1, status: 'ordered', total_amount: 15.98, user_id: 1}),
-        knex('orders').insert({id: 2, status: 'ready for pick-up', total_amount: 19.97, user_id: 1})
-      ]);
-    });
-};
