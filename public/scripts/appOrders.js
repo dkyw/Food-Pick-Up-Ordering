b77e39f58073feb57d958e441b5a22d0e363cc2c
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-28 19:12:22 +0000

    Add name and phone to order in orders page

--- public/scripts/appOrders.js
+++ public/scripts/appOrders.js
@@ -6,10 +6,26 @@ $(() => {
   })
   .done(function(orders) {
     for (order of orders) {
-      var $orderId = $("<h1>").text(order.id);
-      var $orderStatus = $("<h1>").text(`STATUS: ${order.status}`);
-      var $orderTotal = $("<h1>").text(`AMOUNT: $ ${order.total_amount}`);
-      $('.orders').append(`<a href="/orders/${order.id}">ORDER #: ${order.id}</a>`, $orderStatus, $orderTotal);
+      var $orderId = $("<h2>").text(order.id);
+      var $orderStatus = $("<h2>").text(`STATUS: ${order.status}`);
+      var $orderTotal = $("<h2>").text(`AMOUNT: $ ${order.total_amount}`).addClass("orderTotal");
+      var $divider = $("<p>").addClass(`divider ${order.user_id}`).text("===================================");
+      
+      $(".orders").append(`<a href="/orders/${order.id}">ORDER #: ${order.id}</a>`, $orderStatus, $orderTotal, $divider);
     }
   });
+
+  $.ajax({
+    method: "GET",
+    url: "/api/users"
+  })
+  .done(function(users) {
+    for (user of users) {
+      var $username = $("<h3>").text(`Name: ${user.user_name}`);
+      var $userphone = $("<h3>").text(`Contact: ${user.phone_number}`);
+      $(`.${user.id}`).before($username, $userphone); 
+    }
+  });
+
+
 });
