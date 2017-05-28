df15f74076554daedd787893561e451fb2d118f8
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-28 01:18:01 +0000

    Change style

--- public/scripts/appOrders.js
+++ public/scripts/appOrders.js
@@ -9,7 +9,7 @@ $(() => {
       var $orderId = $("<h1>").text(order.id);
       var $orderStatus = $("<h1>").text(`STATUS: ${order.status}`);
       var $orderTotal = $("<h1>").text(`AMOUNT: $ ${order.total_amount}`);
-      $('.orders').append(`<a href=/orders/${$orderId}/message>ORDER #: ${order.id}</a>`, $orderStatus, $orderTotal);
+      $('.orders').append(`<a href="/orders/${order.id}">ORDER #: ${order.id}</a>`, $orderStatus, $orderTotal);
     }
   });
 });
