52728af601171fd6287f6ca21083b69006b6cdc8
Ervin Louie Ong <ervin_louie_ong@yahoo.com>
2017-05-29 19:55:45 +0000

    Use with message.ejs

---
+++ public/scripts/appCalls.js
@@ -0,0 +1,44 @@
+$(() => {
+
+  var orderUrlId = document.URL.toString().slice(29);
+
+  $.ajax({
+    method: "GET",
+    url: "/api/orders_items"
+  })
+  .done(function(orders_items) {
+    for (order of orders_items) {
+      var orderId = $('<h2>').text(`Order #: ${orderUrlId}`);
+      if (order.order_id == orderUrlId) {
+        var itemId = order.item_id
+        var quantity= order.quantity;
+        var itemOrder = $('<h2>').text(`Item: ${itemId}`).addClass(`${itemId}`);
+        var itemQuantity = $('<h2>').text(`Quantity: ${quantity}`);
+      }
+      $('.orders').append(itemOrder,itemQuantity);
+    }
+    $('.orders').before(orderId);
+  });
+
+
+
+  $.ajax({
+    method: "GET",
+    url: "/api/items"
+  })
+  .done(function(items) {
+    for (item of items) {
+      $(`.${item.id}`).text(`Item: ${item.name}`);
+    }
+  });
+
+  $(function() {
+    $(".pickup").click(function(){
+      if (confirm("Do you want to continue?")){
+         $('.pickup').submit();
+      }
+    });
+  });
+
+
+}); //end
